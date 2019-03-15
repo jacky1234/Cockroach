22588b5e116508552d5fe29c6e2bf967aeeb7122
jacky1234 <jackdoson2011@gmail.com>
2019-03-15 18:13:39 +0800

    first commit

---
+++ app/src/main/java/com/wanjian/demo/App.java
@@ -0,0 +1,78 @@
+package com.wanjian.demo;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.wanjian.cockroach.Cockroach;
+import com.wanjian.cockroach.ExceptionHandler;
+import com.wanjian.demo.support.CrashLog;
+import com.wanjian.demo.support.DebugSafeModeTipActivity;
+import com.wanjian.demo.support.DebugSafeModeUI;
+
+/**
+ * Created by wanjian on 2018/5/19.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        install();
+    }
+
+
+    private void install() {
+        final Thread.UncaughtExceptionHandler sysExcepHandler = Thread.getDefaultUncaughtExceptionHandler();
+        final Toast toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+        DebugSafeModeUI.init(this);
+        Cockroach.install(this, new ExceptionHandler() {
+            @Override
+            protected void onUncaughtExceptionHappened(Thread thread, Throwable throwable) {
+                Log.e("AndroidRuntime", "--->onUncaughtExceptionHappened:" + thread + "<---", throwable);
+                CrashLog.saveCrashLog(getApplicationContext(), throwable);
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        toast.setText(R.string.safe_mode_excep_tips);
+                        toast.show();
+                    }
+                });
+            }
+
+            @Override
+            protected void onBandageExceptionHappened(Throwable throwable) {
+                throwable.printStackTrace();//打印警告级别log，该throwable可能是最开始的bug导致的，无需关心
+                toast.setText("Cockroach Worked");
+                toast.show();
+            }
+
+            @Override
+            protected void onEnterSafeMode() {
+                int tips = R.string.safe_mode_tips;
+                Toast.makeText(App.this, getResources().getString(tips), Toast.LENGTH_LONG).show();
+                DebugSafeModeUI.showSafeModeUI();
+
+                if (BuildConfig.DEBUG) {
+                    Intent intent = new Intent(App.this, DebugSafeModeTipActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            protected void onMayBeBlackScreen(Throwable e) {
+                Thread thread = Looper.getMainLooper().getThread();
+                Log.e("AndroidRuntime", "--->onUncaughtExceptionHappened:" + thread + "<---", e);
+                //黑屏时建议直接杀死app
+                sysExcepHandler.uncaughtException(thread, new RuntimeException("black screen"));
+            }
+
+        });
+
+    }
+}
---
+++ app/src/main/java/com/wanjian/demo/MainAct.java
@@ -0,0 +1,136 @@
+package com.wanjian.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+
+/**
+ * Created by wanjian on 2018/1/22.
+ */
+
+public class MainAct extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+
+        findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                throw new RuntimeException("点击异常");
+            }
+        });
+        findViewById(R.id.thread).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        super.run();
+                        throw new RuntimeException("子线程异常");
+                    }
+                }.start();
+            }
+        });
+        findViewById(R.id.handler).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new Handler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        throw new RuntimeException("handler异常");
+                    }
+                });
+            }
+        });
+
+        findViewById(R.id.act).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainAct.this, SecondAct.class));
+            }
+        });
+
+        findViewById(R.id.noact).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainAct.this, UnknowAct.class));
+            }
+        });
+        ////////黑屏测试//////////
+        findViewById(R.id.newActOnCreate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "onCreate");
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.newActOnStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "onStart");
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.newActOnReStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "onRestart");
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.newActOnResume).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "onResume");
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.newActOnPause).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "onPause");
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.newActOnStop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "onStop");
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.newActonDestroy).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "onDestroy");
+                startActivity(intent);
+            }
+        });
+        findViewById(R.id.newActFinish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainAct.this, LifecycleExceptionActivity.class);
+                intent.putExtra(LifecycleExceptionActivity.METHOD, "finish");
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
